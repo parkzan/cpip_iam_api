93b98b166521e2f5edd003e17f43f6cdc522fd68
jamikorn jongka <mmemoriad@gmail.com>
2019-06-07 11:27:40 +0700

    update config -> contextPath, db
    add swagger

--- src/test/java/com/prior/iam/IAMApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.prior.iam;
-
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-public class IAMApplicationTests {
-
-}
