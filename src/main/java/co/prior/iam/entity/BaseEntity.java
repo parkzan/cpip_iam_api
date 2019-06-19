a258d546434d528b066e4dad51150e2535fcae17
Jamikorn <mmemoriad@gmail.com>
2019-06-19 11:50:15 +0700

    impl audit trail

--- src/main/java/co/prior/iam/entity/BaseEntity.java
+++ src/main/java/co/prior/iam/entity/BaseEntity.java
@@ -1,30 +1,55 @@
 package co.prior.iam.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.Setter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PostLoad;
+import javax.persistence.Transient;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Getter
-@Setter
+import lombok.Data;
+
+@Data
 @MappedSuperclass
-public  class BaseEntity {
+public class BaseEntity<T> {
 
     @JsonIgnore
-    protected String isDeleted = "N";
     @CreationTimestamp
-    @JsonIgnore
     protected LocalDateTime createdDate;
 
     @JsonIgnore
-    protected LocalDateTime updatedDate;
+    protected String createdBy = "ADMIN";
+    
     @JsonIgnore
-    protected String createdBy ="ADMIN";
+    @UpdateTimestamp
+    protected LocalDateTime updatedDate;
+    
     @JsonIgnore
     protected String updatedBy;
+    
+    @JsonIgnore
+    protected String isDeleted = "N";
 
+    @JsonIgnore
+    @Transient
+    protected T previousState;
+    
+    @SuppressWarnings("unchecked")
+	@PostLoad
+    private void setPreviousState() throws IOException {
+    	ObjectMapper mapper = new ObjectMapper();
+    	String serialized = mapper.writeValueAsString(this);
+    	this.previousState = (T) mapper.readValue(serialized, this.getClass());
+    }
+
+    public T getPreviousState(){
+    	return previousState;
+    }
+    
 }
