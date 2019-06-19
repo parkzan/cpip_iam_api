159a6923710080db9a58c5c4b22df37f67c53c90
Jamikorn <mmemoriad@gmail.com>
2019-06-19 16:31:15 +0700

    mod entity system, role

--- src/main/java/co/prior/iam/entity/IamMsRole.java
+++ src/main/java/co/prior/iam/entity/IamMsRole.java
@@ -1,41 +1,39 @@
 package co.prior.iam.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.Date;
-
+@Data
+@EqualsAndHashCode(callSuper = false)
 @Entity
 @Table (name = "iam_ms_role")
-@Data
-public class IamMsRole extends BaseEntity{
+public class IamMsRole extends BaseEntity<IamMsRole> {
 
     @Id
     @SequenceGenerator(name = "role_id_seq", sequenceName = "iam_ms_role_role_id_seq",allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_id_seq")
     private Long roleId;
 
-
     private String roleCode;
-
 
     private String roleName;
 
-
     private String roleIcon;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "system_id")
     private IamMsSystem iamMsSystem;
-
-
-
 
 }
--- src/main/java/co/prior/iam/entity/IamMsSystem.java
+++ src/main/java/co/prior/iam/entity/IamMsSystem.java
@@ -1,38 +1,41 @@
 package co.prior.iam.entity;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
-
+@Data
+@EqualsAndHashCode(callSuper = false)
 @Entity
 @Table(name = "iam_ms_system")
-@Data
-public class IamMsSystem extends BaseEntity{
+public class IamMsSystem extends BaseEntity<IamMsSystem> {
+	
     @Id
     @SequenceGenerator(name = "system_id_seq", sequenceName = "iam_ms_system_system_id_seq",allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "system_id_seq")
     private Long systemId;
 
-
     private String systemCode;
-
 
     private String systemName;
 
-
     private String systemIcon;
 
-    @OneToMany(mappedBy = "iamMsSystem" , cascade = CascadeType.ALL)
-    private List<IamMsRole> iamMsRoles ;
-
-
+    @JsonIgnore
+    @OneToMany(mappedBy = "iamMsSystem", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<IamMsRole> iamMsRoles;
+    
 }
