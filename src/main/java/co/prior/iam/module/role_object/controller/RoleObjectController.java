eebbde4f32b6c2a0a95e0ca3b4b39378a87c577f
TeamChii <worapong4712@gmail.com>
2019-06-25 16:58:20 +0700

    RoleObjectController

--- src/main/java/co/prior/iam/module/role_object/controller/RoleObjectController.java
+++ src/main/java/co/prior/iam/module/role_object/controller/RoleObjectController.java
@@ -1,12 +1,9 @@
 package co.prior.iam.module.role_object.controller;
 
 
-import co.prior.iam.entity.IamMsRoleObject;
-import co.prior.iam.module.role_object.model.request.RoleMapObjectCreateReq;
 import co.prior.iam.module.role_object.model.request.RoleObjectEditReq;
 import co.prior.iam.module.role_object.model.respone.RoleMapObjectRespone;
 import co.prior.iam.module.role_object.service.GetRoleObjectService;
-import co.prior.iam.module.role_object.service.RoleObjectCreateService;
 import co.prior.iam.module.role_object.service.RoleObjectEditService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,26 +16,19 @@ import java.util.List;
 @RequestMapping(path = "/api/roleObject")
 public class RoleObjectController {
 
-    private RoleObjectCreateService roleObjectCreateService;
+
     private GetRoleObjectService getRoleObjectService;
     private RoleObjectEditService roleObjectEditService;
 
-    RoleObjectController(RoleObjectCreateService roleObjectCreateService,GetRoleObjectService getRoleObjectService , RoleObjectEditService roleObjectEditService){
+    RoleObjectController(GetRoleObjectService getRoleObjectService , RoleObjectEditService roleObjectEditService){
 
-        this.roleObjectCreateService = roleObjectCreateService;
+
         this.getRoleObjectService = getRoleObjectService;
         this.roleObjectEditService = roleObjectEditService;
 
     }
 
-    @PostMapping
-    public ResponseEntity<Void> createRoleObject(@RequestBody RoleMapObjectCreateReq roleMapObjectCreateReq) throws Exception {
-        log.info("Controller createRoleObject: {}", roleMapObjectCreateReq);
-        roleObjectCreateService.createRoleMapObject(roleMapObjectCreateReq);
 
-        return ResponseEntity.created(null).build();
-
-    }
 
     @GetMapping
     @RequestMapping(path = "/{systemId}/{roleId}")
@@ -62,7 +52,7 @@ public class RoleObjectController {
 
     }
 
-    @PutMapping
+    @PostMapping
     public ResponseEntity<Void> editRoleObject(@RequestBody RoleObjectEditReq roleObjectEditReq) throws Exception{
         log.info("Controller editRoleObject: {}", roleObjectEditReq);
         roleObjectEditService.editRoleObject(roleObjectEditReq);
