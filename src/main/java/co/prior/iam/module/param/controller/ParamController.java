c28f17ea0f2901c462739a4d9f7aa2db5066c455
TeamChii <worapong4712@gmail.com>
2019-06-28 16:05:45 +0700

    param

--- src/main/java/co/prior/iam/module/param/controller/ParamController.java
+++ src/main/java/co/prior/iam/module/param/controller/ParamController.java
@@ -2,8 +2,10 @@ package co.prior.iam.module.param.controller;
 
 
 import co.prior.iam.entity.IamMsParameterGroup;
+import co.prior.iam.module.param.model.ParamGroupCreateModel;
 import co.prior.iam.module.param.model.ParamInfoCreateModel;
 import co.prior.iam.module.param.model.ParamRespone;
+import co.prior.iam.module.param.service.ParamGroupCreateService;
 import co.prior.iam.module.param.service.ParamGroupInquiryService;
 import co.prior.iam.module.param.service.ParamInfoCreateService;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +19,12 @@ public class ParamController {
 
     ParamGroupInquiryService paramGroupInquiryService;
     ParamInfoCreateService paramInfoCreateService;
+    ParamGroupCreateService paramGroupCreateService;
 
-    public ParamController(ParamGroupInquiryService paramGroupInquiryService ,ParamInfoCreateService paramInfoCreateService){
+    public ParamController(ParamGroupInquiryService paramGroupInquiryService ,ParamInfoCreateService paramInfoCreateService , ParamGroupCreateService paramGroupCreateService){
         this.paramGroupInquiryService = paramGroupInquiryService;
         this.paramInfoCreateService = paramInfoCreateService;
+        this.paramGroupCreateService = paramGroupCreateService;
     }
 
 
@@ -32,6 +36,13 @@ public class ParamController {
     @PostMapping("/paramGroups")
     public ResponseEntity<Void> create(@RequestBody ParamInfoCreateModel paramInfoCreateModel) throws Exception{
         paramInfoCreateService.createParamInfo(paramInfoCreateModel);
+
+        return ResponseEntity.created(null).build();
+    }
+
+    @PostMapping("/paramGroup/create")
+    public ResponseEntity<Void> createGroup(@RequestBody ParamGroupCreateModel paramGroupCreateModel) throws Exception{
+        paramGroupCreateService.createParamGroup(paramGroupCreateModel);
 
         return ResponseEntity.created(null).build();
     }
