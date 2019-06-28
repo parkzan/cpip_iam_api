c76d73d742d2ddc062a0c138c6c1fcf45aafebed
TeamChii <worapong4712@gmail.com>
2019-06-28 11:36:48 +0700

    count child object

--- src/main/java/co/prior/iam/module/object/service/ObjectChildInqueryService.java
+++ src/main/java/co/prior/iam/module/object/service/ObjectChildInqueryService.java
@@ -4,6 +4,7 @@ import co.prior.iam.entity.IamMsObject;
 import co.prior.iam.entity.IamMsSystem;
 import co.prior.iam.error.DataNotFoundException;
 import co.prior.iam.model.AnswerFlag;
+import co.prior.iam.module.object.model.respone.ObjectRespone;
 import co.prior.iam.repository.ObjectRepository;
 import co.prior.iam.repository.SystemRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -28,17 +29,51 @@ public class ObjectChildInqueryService {
 	}
 
 	@Transactional
-	public List<IamMsObject> inqueryChildObject(Long systemId, Long objectId) throws Exception {
+	public List<ObjectRespone> inqueryChildObject(Long systemId, Long objectId) throws Exception {
 		log.info("Service inqueryChildObject: {}", objectId);
 
 		List<IamMsObject> listObject = objectRepository.findByIamMsSystem_SystemIdAndObjectParent_ObjectIdAndIsDeleted(
 				systemId, objectId, AnswerFlag.N.toString());
 
+		List<IamMsObject> listAllObject = objectRepository.findByIamMsSystem_SystemIdAndIsDeleted(systemId,
+				AnswerFlag.N.toString());
+
+		List<ObjectRespone> list = new ArrayList<>();
+
 		if (listObject.isEmpty()) {
 			throw new DataNotFoundException("data not found");
 		}
+		for (IamMsObject object : listObject) {
 
-		return listObject;
 
+			long count = 0;
+			ObjectRespone respone = new ObjectRespone();
+			respone.setSystemId(object.getIamMsSystem().getSystemId());
+			respone.setObjectCode(object.getObjectCode());
+			respone.setObjectId(object.getObjectId());
+			respone.setObjectTotalChild(countObjectChild(object, listAllObject, count));
+
+			list.add(respone);
+
+		}
+		return list;
+
+	}
+
+	private long countObjectChild(IamMsObject root , List<IamMsObject> list  , long count ) throws Exception{
+
+		for (IamMsObject obj : list) {
+			if(obj.getObjectParent() != null){
+				if (obj.getObjectParent().getObjectId() == root.getObjectId()) {
+
+					count++ ;
+					countObjectChild(obj, list ,count );
+
+				}
+
+			}
+
+		}
+		return count;
 	}
 }
