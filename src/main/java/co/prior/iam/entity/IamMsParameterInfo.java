489a943751386ff96fdf54d0d924f987878044a7
parkzan <parkzan.papa57@gmail.com>
2019-09-17 09:52:15 +0700

    edit entity paraminfo

--- src/main/java/co/prior/iam/entity/IamMsParameterInfo.java
+++ src/main/java/co/prior/iam/entity/IamMsParameterInfo.java
@@ -1,13 +1,6 @@
 package co.prior.iam.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,6 +22,8 @@ public class IamMsParameterInfo extends BaseEntity<IamMsParameterInfo> {
 	@SequenceGenerator(name = "param_info_id_seq", sequenceName = "iam_ms_parameter_info_seq", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "param_info_id_seq")
 	private Long paramInfoId;
+
+	@Column(name = "param_code")
 	private String paramInfo;
 	private String paramEnDescription;
 	private String paramLocalDescription;
