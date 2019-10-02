5a62f73acee7bdf63b95e00bab902dc08560ca5c
TeamChii <worapong4712@gmail.com>
2019-10-02 15:56:46 +0700

    timzonr

--- src/main/java/co/prior/iam/IAMApplication.java
+++ src/main/java/co/prior/iam/IAMApplication.java
@@ -7,9 +7,17 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableCaching
 public class IAMApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IAMApplication.class, args);
