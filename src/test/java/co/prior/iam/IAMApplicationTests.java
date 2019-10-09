17ba41a145c10be0d04b2a81e9ad31d8b50c2be2
TeamChii <worapong4712@gmail.com>
2019-10-09 15:54:06 +0700

    timezone

--- src/test/java/co/prior/iam/IAMApplicationTests.java
+++ src/test/java/co/prior/iam/IAMApplicationTests.java
@@ -7,14 +7,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.TimeZone;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class IAMApplicationTests {
 
 	@Test
 	public void contextLoads() {
 
-
+		System.out.print(TimeZone.getDefault());
 	}
 	
 }
